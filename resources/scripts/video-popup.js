2376099429e52584e0179af70fd761671fcca971
IsadchenkoJulia <IsadchenkoJulia@yandex.ru>
2017-05-30 16:29:58 +0300

    Fixes, Video in modal window

---
+++ resources/scripts/video-popup.js
@@ -0,0 +1,23 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubePlayerAPIReady() {
+    player = new YT.Player('youtubePlayer', {
+        height: '360',
+        width: '640',
+        videoId: '0FoTstucf9U'
+    });
+}
+
+$("[href='#mediaPopup']").on("click", function() {
+    $(".overlay").fadeIn();
+    player.playVideo();
+});
+
+$(".overlay").on("click", function() {
+    $(this).fadeOut();
+    player.pauseVideo();
+});
